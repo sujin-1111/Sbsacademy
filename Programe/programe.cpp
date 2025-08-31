21cc0863eccf33161cfb35b26439cb44660c2c0f
sujin-1111 <sujin3421192@gmail.com>
2025-09-01 03:53:40 +0900

    의사 난수

--- Programe/programe.cpp
+++ Programe/programe.cpp
@@ -1,4 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void shuffle(int list[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		int seed = rand() % size;
+
+		int temporary = list[seed];
+
+		list[seed] = list[i];
+
+		list[i] = temporary;
+	}
+
+}
 
 int main()
 {
@@ -8,16 +25,32 @@ int main()
 	// UTC 기준으로 1970년 1월 1일 0시 0분 초부터 경과된
 	// 시간을 초(sec)로 반환하는
 
+	// srand(time(NULL));
+	// 
+	// int random = rand() % 10 + 1;
+	// 
+	// printf("random : %d\n", random);
+
+#pragma endregion
 
 
+#pragma region Shuffle Function
+
+	srand(time(NULL));
+
+	int list[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+	int size = sizeof(list) / sizeof(list[0]);
+
+	shuffle(list, size);
+
+	for (int i = 0; i < size; i++)
+	{
+		printf("list[%d] = %d\n", i, list[i]);
+	}
 
 
-
-
-
-
-
-
+#pragma endregion
 
 	return 0;
 }
